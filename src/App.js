f26f3c36cbb9a9261b28d752f88614d08a1de09b
amelsemic <semicamel@hotmail.com>
2022-07-12 20:35:50 +0200

    re-rendering fixes

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [newExpense, setNewExpense] = useState(false)
+
+    useEffect(() => {
+      const fetchExpenses = async () => {
+        const response = await fetch(
+          "https://expenses-tracker-17e92-default-rtdb.firebaseio.com/requests.json"
+        );
+        const data = await response.json();
+        const FBexpenses = [];
+        for (const key in data) {
+          const FBexpense = {
+            id: key,
+            title: data[key].title,
+            amount: data[key].amount,
+            date: data[key].date,
+          };
+          FBexpenses.push(FBexpense);
+        }
+  
+        setExpenses(FBexpenses);
+      };
+      fetchExpenses();
+      setNewExpense(false)
+    }, [newExpense]);
+
+  const newExpenseHandler = () =>{
+    setNewExpense(true);
+  }
+
+  return (
+    <div>
+      <NewExpense onNewExpense={newExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && <ExpenseForm onNewExpense={props.onNewExpense} onCancel={stopEditingHandler} />}
+    </div>
+  );
+};
+
+export default NewExpense;
